47f45603ff9c43e105cc7e9ecbb11e42331ce12c
trevdevv <trevorjperez1@gmail.com>
2018-03-26 19:00:12 -0700

    Added more info to cards section and the modals are now functional

--- Contact_Form_Webpage/index.php
+++ Contact_Form_Webpage/index.php
@@ -107,9 +107,9 @@
                     <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
                         <div class="card-header">Project 1</div>
                         <div class="card-body">
-                            <h4 class="card-title">Primary card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#exampleModal">Learn More</button>
+                            <h4 class="card-title">Tic Tac Toe Game (Minmax)</h4>
+                            <p class="card-text">Used the notion of Artifical Intelligence by incorporting the famous Minmax function.</p>
+                            <button type="button" class="btn btn-secondar" data-toggle="modal" data-target="#modal1">Learn More</button>
                             <!-- Button trigger modal -->
                         </div>
                     </div>
@@ -118,9 +118,9 @@
                     <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
                         <div class="card-header">Project 2</div>
                         <div class="card-body">
-                            <h4 class="card-title">Primary card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <button type="button" class="btn btn-secondar" data-toggle="modal" data-target="modal-2">Learn More</button>
+                            <h4 class="card-title">Card game War <br>IOS App</h4>
+                            <p class="card-text">Using the Swift programming language and the xCode IDE, I created the famous card game 'WAR'. </p>
+                            <button type="button" class="btn btn-secondar" data-toggle="modal" data-target="#modal2">Learn More</button>
                         </div>
                     </div>
                 </div>
@@ -128,9 +128,9 @@
                     <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
                         <div class="card-header">Project 3</div>
                         <div class="card-body">
-                            <h4 class="card-title">Primary card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="modal-3">Learn More</button>
+                            <h4 class="card-title">Selling Tires <br> Customer Web Site</h4>
+                            <p class="card-text">I maximized my customers profits by nearly 50% by creating them an amazing web site.</p>
+                            <button type="button" class="btn btn-secondar" data-toggle="modal" data-target="#modal3">Learn More</button>
                         </div>
                     </div>
                 </div>
@@ -139,8 +139,50 @@
     </section>
 
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal 1 -->
+<div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 2 -->
+<div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 3 -->
+<div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
