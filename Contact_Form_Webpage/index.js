e54297cd5ce4a96aa28c8cb25d504f0fb5b39f85
trevdevv <trevorjperez1@gmail.com>
2018-03-26 18:31:27 -0700

    Added down arrow animation effect. Added full version of jquery for annimate function. styled the down arrow

--- Contact_Form_Webpage/index.js
+++ Contact_Form_Webpage/index.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
 	setTimeout(function() {
 		$("#main").removeClass("is-loading");
 	}, 100)
+
+
+	$("#arw-btn").click(function() {
+		$('html, body').animate({
+				scrollTop: $("#projects").offset().top
+		}, 2000);
+	});
+
+
 });
+
+
+
+
+
+
+
--- Contact_Form_Webpage/index.php
+++ Contact_Form_Webpage/index.php
@@ -94,10 +94,13 @@
         <h1 id = "cover-title">
             Trev Devv Media
         </h1>
+        <div id="header-down-arrow" class = "arrow">
+            <a id = "arw-btn" href="#projects"><span></span></a>
+        </div>
     </div>
     <br>
     <!-- Section -->
-    <section>
+    <section id = "projects">
         <div class="container">
             <div class="row">
                 <div class="col-sm-4">
@@ -158,7 +161,7 @@
 </div>
 <br>
     <!-- Footer -->
-    <footer>
+    <footer id = "contact-form">
         <!-- Contact Form -->
         <div class="container">	
             <?php if($msg != ''): ?>
@@ -183,7 +186,8 @@
         </div>
     </footer>
     <!-- Bootstrap JS&Jquery Script -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="index.js"></script>
